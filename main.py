0b7e18fc488513fec17ebfa1cd55dbe14584fe7c
master010410 <master010410@outlook.com>
2025-02-03 18:13:00 -0800

    CT-3: manage env variables

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import uvicorn
 
+from app.config.settings import settings
+
 if __name__ == "__main__":
-    uvicorn.run("app.app:app", reload=True)
+    uvicorn.run("app.app:app", reload=True, port=settings.port, host=settings.host)
