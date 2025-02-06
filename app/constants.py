1dee4a364812c9113325172f49390b6d60424a0f
master010410 <master010410@outlook.com>
2025-02-06 10:13:01 -0800

    md-10: add dev environment

---
+++ app/constants.py
@@ -0,0 +1,6 @@
+import enum
+
+
+class MODE(enum.StrEnum):
+    DEV = "development"
+    PROD = "production"
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import uvicorn
 
 from app.config.settings import settings
+from app.constants import MODE
 
 if __name__ == "__main__":
     uvicorn.run(
         "app.app:app",
-        reload=True,
+        reload=settings.mode == MODE.DEV,
         port=settings.port,
         host=settings.host,
         log_config=settings.logging_config,
